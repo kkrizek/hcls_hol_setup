edcb82ed3e7939082c51066a3970aeb492f3662d
kkrizek <kkrizek70@hotmail.com>
2024-09-17 16:49:46 -0500

    Update setup_test.sql

--- setup_test.sql
+++ setup_test.sql
@@ -1,6 +1,5 @@
 --!jinja
 use warehouse hol_setup_wh;
---set my_var = {{hol_prefix}};
-set my_var = 1;
+set my_var = '{{hol_prefix}}';
 select $my_var;
 --insert into hcls_hol.hol.script_test(col1) values ('hol_prefix');
