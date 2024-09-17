bdedd1759f80fb78c27559d80988478c45c98cea
kkrizek <kkrizek70@hotmail.com>
2024-09-17 16:40:17 -0500

    Update setup_test.sql

--- setup_test.sql
+++ setup_test.sql
@@ -1,3 +1,7 @@
 use warehouse hol_setup_wh;
 
-insert into hcls_hol.hol.script_test(col1) values ('hol_prefix');
+set my_var = {{hol_prefix}};
+
+select $my_var;
+
+--insert into hcls_hol.hol.script_test(col1) values ('hol_prefix');
