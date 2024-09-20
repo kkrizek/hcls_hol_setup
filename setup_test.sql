5f921ee2f7269833c16917d6a8ce4e08f9e79ad7
kkrizek <kkrizek70@hotmail.com>
2024-09-20 08:35:36 -0500

    Update setup_test.sql

--- setup_test.sql
+++ setup_test.sql
@@ -8,10 +8,16 @@ declare
   v_database varchar;
   v_schema varchar;
   v_streamlit_apps variant;
+  lab_pwd varchar;
+  qry varchar;
 begin
   select hol_name, hol_description, hol_date, nbr_users, database, schema, streamlit_apps         
     into :v_hol_name, :v_hol_description, :v_hol_date, :v_nbr_users, :v_database, :v_schema, :v_streamlit_apps
     from hcls_hol.hol.hol_definition
    where hol_prefix = '{{hol_prefix}}';
-  return :v_hol_name;
+
+  set lab_pwd := 'Sn@wpark!phd';
+  set qry := 'GRANT EXECUTE TASK ON ACCOUNT TO ROLE ' + {{hol_prefix}} + 'XXX';
+
+  return :qry;
 end;
