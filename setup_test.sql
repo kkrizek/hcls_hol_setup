e6700ce9db2734bd2af334251ffef731a500a564
kkrizek <kkrizek70@hotmail.com>
2024-09-20 08:38:14 -0500

    Update setup_test.sql

--- setup_test.sql
+++ setup_test.sql
@@ -1,23 +1,25 @@
 --!jinja
 
 declare
-  v_hol_name varchar;
-  v_hol_description varchar;
-  v_hol_date date;
-  v_nbr_users integer;
-  v_database varchar;
-  v_schema varchar;
-  v_streamlit_apps variant;
+  i_hol_prefix varchar;
+  hol_name varchar;
+  hol_description varchar;
+  hol_date date;
+  nbr_users integer;
+  database varchar;
+  schema varchar;
+  streamlit_apps variant;
   lab_pwd varchar;
   qry varchar;
 begin
+  set i_hol_prefix := '{{hol_prefix}}'
   select hol_name, hol_description, hol_date, nbr_users, database, schema, streamlit_apps         
-    into :v_hol_name, :v_hol_description, :v_hol_date, :v_nbr_users, :v_database, :v_schema, :v_streamlit_apps
+    into :hol_name, :hol_description, :hol_date, :nbr_users, :database, :schema, :streamlit_apps
     from hcls_hol.hol.hol_definition
-   where hol_prefix = '{{hol_prefix}}';
+   where hol_prefix = i_hol_prefix;
 
   set lab_pwd := 'Sn@wpark!phd';
-  set qry := 'GRANT EXECUTE TASK ON ACCOUNT TO ROLE ' + '{{hol_prefix}}' + 'XXX';
+  set qry := 'GRANT EXECUTE TASK ON ACCOUNT TO ROLE ' + i_hol_prefix + 'XXX';
 
   return :qry;
 end;
