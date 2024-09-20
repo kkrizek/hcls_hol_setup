a92ed7e861b5828aa61d66d306c1170867a67981
kkrizek <kkrizek70@hotmail.com>
2024-09-20 08:19:32 -0500

    Update setup_test.sql

--- setup_test.sql
+++ setup_test.sql
@@ -9,7 +9,8 @@ declare
   v_schema varchar;
   v_streamlit_apps variant;
 begin
-  select hol_name         into :v_hol_name
+  select hol_name, hol_description, hol_date, nbr_users, database, schema, streamlit_apps         
+    into :v_hol_name, :v_hol_description, :v_hol_date, :v_nbr_users, :v_database, :v_schema, :v_streamlit_apps
     from hcls_hol.hol.hol_definition
    where hol_prefix = '{{hol_prefix}}';
   return :v_hol_name;
