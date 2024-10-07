fd3a6c641f0243c47d6a893d323e7fab74677fe8
kkrizek <kkrizek70@hotmail.com>
2024-10-07 11:57:10 -0500

    Update hol_setup.sql

--- hol_setup.sql
+++ hol_setup.sql
@@ -1,2 +1,5 @@
 create database if not exists hol_test_setup;
 create schema if not exists hol_test_setup.hol;
+
+create or replace role hol_setup_script_role;
+create or replace warehouse hol_setup_script_wh warehouse_size = 'XSMALL';
