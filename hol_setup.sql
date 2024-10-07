d29a78ab86b256132aa6f0c923882496601f852e
kkrizek <kkrizek70@hotmail.com>
2024-10-07 12:13:35 -0500

    Update hol_setup.sql

--- hol_setup.sql
+++ hol_setup.sql
@@ -1,4 +1,4 @@
-dcreate database if not exists hol_test_setup;
+create database if not exists hol_test_setup;
 create schema if not exists hol_test_setup.hol;
 
 create or replace role hol_setup_script_role;
