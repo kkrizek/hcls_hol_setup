fbd76b2350bf6e7f65206fbee45fe26c4d2d3d95
kkrizek <kkrizek70@hotmail.com>
2024-10-07 12:11:42 -0500

    Update hol_setup.sql

--- hol_setup.sql
+++ hol_setup.sql
@@ -1,7 +1,7 @@
-create database if not exists hol_test_setup;
+dcreate database if not exists hol_test_setup;
 create schema if not exists hol_test_setup.hol;
 
 create or replace role hol_setup_script_role;
-create or replace warehouse hol_setup_script_wh warehouse_size = 'XSMALL' austo_suspend = 120 auto_resume = TRUE initially_suspended=TRUE;
+create or replace warehouse hol_setup_script_wh warehouse_size = 'XSMALL' auto_suspend = 120 auto_resume = TRUE initially_suspended=TRUE;
 
 
